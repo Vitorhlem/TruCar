2242578ad3ce9fc4d6eb91e3aba0f9816570bec1
Vitorhlem <Vitorhugolemes6@gmail.com>
2025-08-28 12:27:17 -0300

    backup

--- backend/app/models/implement_model.py
+++ backend/app/models/implement_model.py
@@ -1,9 +1,13 @@
 # backend/app/models/implement_model.py
-
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
+
+class ImplementStatus(str, enum.Enum):
+    AVAILABLE = "available"
+    IN_USE = "in_use"
+    MAINTENANCE = "maintenance"
 
 class Implement(Base):
     __tablename__ = "implements"
@@ -12,6 +16,9 @@ class Implement(Base):
     name = Column(String(100), nullable=False)
     brand = Column(String(50), nullable=False)
     model = Column(String(50), nullable=False)
+    type = Column(String, nullable=True) # Ex: "Arado", "Plantadeira"
+    status = Column(Enum(ImplementStatus), default=ImplementStatus.AVAILABLE)
+
     year = Column(Integer, nullable=False)
     identifier = Column(String(50), unique=True, nullable=True) # Ex: número de série ou patrimônio
 
