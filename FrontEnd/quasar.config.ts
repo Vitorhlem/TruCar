b1d3e28e467dfe1414db9a509d38cea3fce67b51
Vitorhlem <Vitorhugolemes6@gmail.com>
2025-09-30 19:40:28 -0300

    fix: add appID for android build in quasar.config.ts

--- FrontEnd/quasar.config.ts
+++ FrontEnd/quasar.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25e066dcab545d5190435625c433e6dad2b6a12893b9c5da3fdc7c4c9e2d3739
-size 7335
+oid sha256:8ce845cb026c80d1376084e3499cfc71576230c73c3dd973ac7a5fe2cee27f0b
+size 7345
