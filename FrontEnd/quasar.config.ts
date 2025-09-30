8cd9f6aac741599900238cf15c83dd969dd1fff7
Vitorhlem <Vitorhugolemes6@gmail.com>
2025-09-30 19:37:00 -0300

    fix: add appID for android build in quasar.config.ts

--- FrontEnd/quasar.config.ts
+++ FrontEnd/quasar.config.ts
@@ -173,6 +173,10 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor
     capacitor: {
       hideSplashscreen: true,
+      // *** ADICIONADO AQUI ***
+      // Define o ID do App para evitar a pergunta no CI/CD
+      packageName: 'com.trucar.app',
+      appName: 'TruCar'
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/configuring-electron
