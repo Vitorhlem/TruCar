e12390e9508f9adfa88c1b4e8f071a359fc0f955
Vitorhlem <Vitorhugolemes6@gmail.com>
2025-09-30 19:48:23 -0300

    add src-capacitor for android build

--- FrontEnd/quasar.config.ts
+++ FrontEnd/quasar.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ce845cb026c80d1376084e3499cfc71576230c73c3dd973ac7a5fe2cee27f0b
-size 7345
+oid sha256:1443f3ece4928e0a2742559c662ac732cad7c375390da6372ca4888a7751feec
+size 7181
