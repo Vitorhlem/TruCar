8667e1e97e0c6e196fa106d85bdc43e211ea1daa
vitorhlem <vitorhugolemes6@gmail.com>
2025-11-25 11:15:42 -0300

    Export {api}

--- src-client/src/services/api.ts
+++ src-client/src/services/api.ts
@@ -3,19 +3,21 @@ import axios from 'axios';
 
 // Esta função determina a URL base da API com base no ambiente
 const getBaseURL = () => {
-  // 'process.env.DEV' é uma variável booleana injetada pelo Quasar
-  // que é 'true' quando você roda o projeto com 'quasar dev'
-  if (process.env.DEV) {
-    return 'http://localhost:8000'; // URL para desenvolvimento local
-  }
-  // Para qualquer outro caso (como ao gerar o build para produção),
-  // ele usará a URL do servidor na nuvem.
-  return 'https://trucar.onrender.com'; // URL para produção
+ // 'process.env.DEV' é uma variável booleana injetada pelo Quasar
+ // que é 'true' quando você roda o projeto com 'quasar dev'
+ if (process.env.DEV) {
+  return 'http://localhost:8000'; // URL para desenvolvimento local
+ }
+ // Para qualquer outro caso (como ao gerar o build para produção),
+ // ele usará a URL do servidor na nuvem.
+ return 'https://trucar.onrender.com'; // URL para produção
 };
 
 const api = axios.create({
-  baseURL: getBaseURL(),
-  withCredentials: true,
+ baseURL: getBaseURL(),
+ withCredentials: true,
 });
 
 export default api;
+// EXPORTAÇÃO NOMEADA ADICIONADA: O arquivo de boot precisa disso.
+export { api };
